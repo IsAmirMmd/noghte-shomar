68637c22df2bef154ef79dd8843db9ac80ab5d31
اَمیرمَمّد <amirmmd.ltf@stu.nit.ac.ir>
2023-08-10 18:42:25 +0330

    adding style and some elements

--- src/App.js
+++ src/App.js
@@ -3,10 +3,22 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h4>
-        خب عزیز دل،<span style={{ color: "#353535" }}>سلام</span>
-      </h4>
-      <input type="text" value={null} />
+      <h1>
+        خب عزیز دلم!
+        <span>سلام</span>
+        <span style={{ color: "#9d174d" }}>♥</span>
+      </h1>
+      <form>
+        <textarea type="text" value={null}></textarea>
+        <button className="btn">بشمار!</button>
+      </form>
+      <div className="counter">
+        <p>تعداد نقطه ها:</p>
+        <span className="dot-count">0</span>
+      </div>
+      <footer>
+        made with <span>♥</span> by <span>IsAmirMmd</span>
+      </footer>
     </div>
   );
 }
