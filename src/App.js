8e8f069fadb0f420074a7230ddf3bbb9ea4d6c06
اَمیرمَمّد <amirmmd.ltf@stu.nit.ac.ir>
2023-08-10 18:10:46 +0330

    adding elements in App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h4>
+        خب عزیز دل،<span style={{ color: "#353535" }}>سلام</span>
+      </h4>
+      <input type="text" value={null} />
     </div>
   );
 }
